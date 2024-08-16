984fc3d46789d4660490c88e8060363f7899102b
emilly29 <103429686+emilly29@users.noreply.github.com>
2024-08-16 09:00:54 -0300

    .

---
+++ script.js
@@ -0,0 +1,43 @@
+const  caixaPrincipal  =  documento . querySelector ( ".caixa-principal" ) ;
+const  caixaPerguntas  =  documento . querySelector ( ".caixa-perguntas" ) ;
+const  caixaAlternativas  =  documento . querySelector ( ".caixa-alternativas" ) ;
+const  caixaResultado  =  documento . querySelector ( ".caixa-resultado" ) ;
+const  textoResultado  =  documento . querySelector ( ".texto-resultado" ) ;
+
+const  perguntas  =  [
+    {
+        enunciado : "Pergunta 1" ,
+        alternativas : [
+            "Alternativa 1" ,
+            "Alternativa 2"
+        ]
+    } ,
+    {
+        enunciado : "Pergunta 2" ,
+        alternativas : [
+            "Alternativa 1" ,
+            "Alternativa 2"
+        ]
+    } ,
+    {
+        enunciado : "Pergunta 3" ,
+        alternativas : [
+            "Alternativa 1" ,
+            "Alternativa 2"
+        ]
+    } ,
+    {
+        enunciado : "Pergunta 4" ,
+        alternativas : [
+            "Alternativa 1" ,
+            "Alternativa 2"
+        ]
+    } ,
+    {
+        enunciado : "Pergunta 5" ,
+        alternativas : [
+            "Alternativa 1" ,
+            "Alternativa 2"
+        ]
+    }
+] ;
